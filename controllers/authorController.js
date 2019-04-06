43ce52ffa088db1bd041fda6d6c158250734be2c
Naukhez Ali <naukhez.3@hotmail.co.uk>
2019-04-06 19:19:23 +0100

    Completed library applications basic features, can now view a list of books and authors data from DB, Next task is to add angular to front end

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,13 +1,53 @@
 var Author = require('../models/author');
+var async = require('async');
+var Book = require('../models/book');
 
-// Display list of all authors
-exports.author_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author list');
-};
+// // Display list of all authors
+// exports.author_list = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Author list');
+// };
 
-// Display detail page for a specific Author
-exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+// Display list of all Authors.
+exports.author_list = function(req, res, next) {
+
+    Author.find()
+      .sort([['family_name', 'ascending']])
+      .exec(function (err, list_authors) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('author_list', { title: 'Author List', author_list: list_authors });
+      });
+  
+  };
+
+// // Display detail page for a specific Author
+// exports.author_detail = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+// };
+
+// Display detail page for a specific Author.
+exports.author_detail = function(req, res, next) {
+
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id)
+              .exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.params.id },'title summary')
+          .exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); } // Error in API usage.
+        if (results.author==null) { // No results.
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render.
+        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
+    });
+
 };
 
 // Display Author create form on GET.
--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,13 +1,52 @@
 var BookInstance = require('../models/bookInstance');
+var Handlebars = require('handlebars');
+// // Display list of all BookInstances.
+// exports.bookinstance_list = function(req, res) {
+//     res.send('NOT IMPLEMENTED: BookInstance list');
+// };
+
+// Block conditional helper
+Handlebars.registerHelper('ifCond', function(v1, v2, options){
+    if(v1 === v2){
+        return options.fn(this);
+    }
+    return options.inverse(this);
+});
 
 // Display list of all BookInstances.
-exports.bookinstance_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance list');
-};
+exports.bookinstance_list = function(req, res, next) {
+
+    BookInstance.find()
+      .populate('book')
+      .exec(function (err, list_bookinstances) {
+        if (err) { return next(err); }
+        // Successful, so render
+        res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances });
+      });
+      
+  };
+
+// // Display detail page for a specific BookInstance.
+// exports.bookinstance_detail = function(req, res) {
+//     res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+// };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function(req, res, next) {
+
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so render.
+      res.render('bookinstance_detail', { title: 'Book:', bookinstance:  bookinstance});
+    })
+
 };
 
 // Display BookInstance create form on GET.
